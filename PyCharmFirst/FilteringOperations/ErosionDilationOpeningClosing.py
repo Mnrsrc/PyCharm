cc47a2f0e167146520c6a24768f3298d9c05a2fd
Munire <51300375+Mnrsrc@users.noreply.github.com>
2020-02-15 22:19:34 +0200

    Erosion, dilation, opening, closing

--- PyCharmFirst/FilteringOperations/ErosionDilationOpeningClosing.py
+++ PyCharmFirst/FilteringOperations/ErosionDilationOpeningClosing.py
@@ -10,16 +10,24 @@ upSkinColor=np.array([33,255,255])
 hsvMaskSkin=cv2.inRange(hsv,lowSkinColor,upSkinColor)
 #Comparision Bitwise
 BitwiseHsvSkin=cv2.bitwise_and(image,image,mask=hsvMaskSkin)
+#Create kernel matris
+kernel=np.ones((5,5),np.uint8)
 #Erosion Processing
-"""kernel=np.ones((5,5),np.uint8)
-erosion=cv2.morphologyEx(image,kernel,iterations=1)"""
+erosion=cv2.erode(hsvMaskSkin,kernel,iterations=1)
 #Dilation Processing
+dilation=cv2.dilate(hsvMaskSkin,kernel,iterations=1)
 #Opening
+opening=cv2.morphologyEx(hsvMaskSkin,cv2.MORPH_OPEN,kernel)
 #Closing
+closing=cv2.morphologyEx(hsvMaskSkin,cv2.MORPH_CLOSE,kernel)
 #Show result
 cv2.imshow("Original",image)
 cv2.imshow("hsvMaskSkin",hsvMaskSkin)
 cv2.imshow("BitwiseHsvSkin",BitwiseHsvSkin)
+cv2.imshow("Erosion", erosion)
+cv2.imshow("Dilation",dilation)
+cv2.imshow("Opening",opening)
+cv2.imshow("Closing",closing)
 #Close all windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
